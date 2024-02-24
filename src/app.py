96a923e7b62defb4407a4fffb9859e889aec78f0
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-02-24 04:22:00 +0000

    feat: few rows

--- src/app.py
+++ src/app.py
@@ -39,11 +39,85 @@ def sitemap():
     return generate_sitemap(app)
 
 
-@app.route('/user', methods=['GET'])
+@app.route('/hello', methods=['GET'])
 def handle_hello():
     response_body = {"message": "Hello, this is your GET /user response "}
     return jsonify(response_body), 200
 
+
+""" 
+Creamos nuestros endpoints
+"""
+@app.route('/users', methods=['GET', 'POST'])
+def handle_users():
+    if request.method == 'GET':
+        response_body = {}
+        results = {}
+        users = db.session.execute(db.select(Users)).scalars()
+        # Opción 1 - For-in
+        list_usuarios = []
+        for row in users:
+            list_usuarios.append(row.serialize())
+        results['users'] = list_usuarios
+        # Opción 2 - List Comprehesion
+        # results['users'] = [row.serialize() for row in users]
+        response_body['message'] = 'Listado de Usuarios'
+        response_body['results'] = results
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json
+        response_body = {}
+        # Escribir la lógica para guardar el registro en la DB
+        user = Users(email = data.get('email'),
+                     password = data.get('password'),
+                     is_active = True)
+        db.session.add(user)
+        db.session.commit()
+        response_body['user'] = user.serialize()
+        return response_body, 200
+
+
+@app.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_user(user_id):
+    response_body = {}
+    results = {}
+    if request.method == 'GET':
+        # opcion 1
+        user = db.session.get(Users, user_id)
+        if not user:
+            response_body['message'] = 'Usuario inexistente'
+            return response_body, 404
+        """
+        # opción 2
+        user = db.one_or_404(db.select(Users).filter_by(id=user_id), 
+                             description=f"User not found , 404")
+        """
+        results['user'] = user.serialize()
+        response_body['message'] = 'Usuario encontrado'
+        response_body['results'] = results
+        return response_body, 200
+    if request.method == 'PUT':
+        data = request.json
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if not user:
+            response_body['message'] = 'Usuario inexistente'
+            return response_body, 404        
+        user.email = data.get('email')
+        db.session.commit()
+        results['user'] = user.serialize()
+        response_body['message'] = 'Usuario actualizado'
+        response_body['results'] = results
+        return response_body, 200
+    if request.method == 'DELETE':
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if not user:
+            response_body['message'] = 'Usuario inexistente'
+            return response_body, 404        
+        db.session.delete(user)
+        db.session.commit()
+        response_body['message'] = 'Usuario eliminado'
+        return response_body, 200
+    
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,12 +9,110 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    subscrition_date = db.Column(db.Date)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         # do not serialize the password, its a security breach
         return {"id": self.id,
                 "email": self.email,
-                'is_active': self.is_active}
+                'is_active': self.is_active,
+                'suscription_date': self.subscrition_date}
+
+
+class Profiles(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    # 2.2. Atributos del modelo. Tipo de dato(longitud), acepta datos vacíos?, es un dato único?
+    firstname = db.Column(db.String(20), nullable=False)
+    lastname = db.Column(db.String(20), nullable=False)
+    nickname =  db.Column(db.String(20), nullable=False)
+    image_url =  db.Column(db.String(120), nullable=False)
+    # 2.3. Clave foránea. Tipo de dato, db.ForeignKey('alias.id')
+    # 3.3 One to One
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True)
+    # 3. Relaciones. db.relationship(Models)
+    users = db.relationship(Users)
+
+    def __repr__(self):
+        return '<Profile: %r>' % self.firstname
+
+    def serialize(self):
+        return {"id": self.id, 
+                "firstname": self.firstname,
+                "lastname": self.lastname,
+                "nickname": self.nickname,
+                "image_url": self.image_url,
+                "users_id": self.users_id}
+
+
+class Addresses(db.Model):
+    __tablename__ = 'addresses'
+    id = db.Column(db.Integer, primary_key=True)
+    street_name = db.Column(db.String(250))
+    street_number = db.Column(db.String(250))
+    post_code = db.Column(db.String(250), nullable=False)
+    # 3.3 One to Many
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    users = db.relationship(Users)
+
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    # 2. Columnas, 
+    # 2.1. Clave primaria. Tipo de dato, primary_key=True
+    id = db.Column(db.Integer, primary_key=True)
+    # 2.2. Atributos del modelo. Tipo de dato(longitud), acepta datos vacíos?, es un dato único?
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer)
+    mass = db.Column(db.String)
+    hair_color = db.Column(db.String)
+    skin_color = db.Column(db.String)
+    eye_color = db.Column(db.String)
+    birth_year = db.Column(db.String)
+    gender = db.Column(db.String)
+    homeworld = db.Column(db.String)
+    url = db.Column(db.String)
+
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    diameter = db.Column(db.Integer)
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    gravity = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+    climate = db.Column(db.Integer)
+    terrain = db.Column(db.Integer)
+    surface_water = db.Column(db.Integer)
+    url = db.Column(db.Integer)
+
+
+class FavoriteCharacters(db.Model):
+    # 1. Creamos el alias de la tabla __tablename__ . Naming convention: snake_case
+    __tablename__ = 'favorite_characters'
+    # 2. Columnas, 
+    # 2.1. Clave primaria. Tipo de dato, primary_key=True
+    id = db.Column(db.Integer, primary_key=True)
+    # 2.3. Clave foránea. Tipo de dato, db.ForeignKey('alias.id')
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id')) # int fk >-< users.id
+    characters_id  = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    # 3. Relaciones. db.relationship(Models)
+    users = db.relationship(Users)
+    characters = db.relationship(Characters)
+
+
+class FavoritePlanets(db.Model):
+    __tablename__ = 'favorite_planets'
+    id = db.Column(db.Integer, primary_key=True)
+    # 3.3 Many to Many
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    users = db.relationship(Users)
+    planets = db.relationship(Planets)
