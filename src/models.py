e641b12cf949b0f4d46244aecf43ba6fcfed16bf
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-02-29 17:23:18 +0000

    feat: models.py & admin.py

--- src/models.py
+++ src/models.py
@@ -50,3 +50,65 @@ class Students(db.Model):
         return {"id": self.id,
                 "name": self.name,
                 'user_id': self.user_id}
+
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<Character: {self.id} - {self.name}>'
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name}
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<Planet: {self.id} - {self.name}>'
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name}
+
+
+class CharacterFavorites(db.Model):
+    __tablename__ = 'character_favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    # Columns FK
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    # Relationships
+    users = db.relationship('Users', foreign_keys=[user_id])
+    characters = db.relationship('Characters', foreign_keys=[character_id])
+
+    def __repr__(self):
+        return f'<Favorites: {self.id} - User: {self.user_id} - Character: {self.character_id}>'
+
+    def serialize(self):
+        return {"id": self.id,
+                "user_id": self.user_id,
+                'character_id': self.character_id}
+
+
+class PlanetFavorites(db.Model):
+    __tablename__ = 'planet_favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    # Columns FK
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    # Relationships
+    users = db.relationship('Users', foreign_keys=[user_id])
+    planets = db.relationship('Planets', foreign_keys=[planet_id])
+
+    def __repr__(self):
+        return f'<Favorites: {self.id} - User: {self.user_id} - Planet: {self.planet_id}>'
+
+    def serialize(self):
+        return {"id": self.id,
+                "user_id": self.user_id,
+                'planet_id': self.planet_id}
